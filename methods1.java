987b98c66302ae8ba2fd567e6a9fdb43b709df6d
Amit333-coder <59141005+Amit333-coder@users.noreply.github.com>
2021-01-02 17:49:44 +0530

    Add files via upload

---
+++ methods1.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    
+    // create strings
+    String first = "Amit";
+    String second = "Sharma";
+
+    // print strings
+    System.out.println(first);   // print Amit
+    System.out.println(second);  // print Sharma
+  }
+}
